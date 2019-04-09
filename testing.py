8cd4ec47855020c4e526e8e36fe56cbdfd76adab
Dan Tenenbaum <dtenenba@fhcrc.org>
2019-04-09 11:25:51 -0700

    converted to use unittest library

--- testing.py
+++ testing.py
@@ -4,46 +4,47 @@
 Tests to make sure that the fh_disable_network_modification policy
 indeed prevents users from invoking network operations.
 """
+import unittest
 
 import boto3
 import botocore
 
 
-def main():
-    "do the work"
+class TestRoleCannotDoNetworkStuff(unittest.TestCase):
+    "test cases"
 
-    # start w/credentials in environment tied to fred hutch account
-    sts = boto3.client("sts")
+    def test_cannot_create_vpc(self):
+        "do the work"
 
-    # assume role in cortex production account
-    print("Assuming role in cortex production account...")
-    resp = sts.assume_role(
-        RoleArn="arn:aws:iam::329997391649:role/admin-with-limited-network",
-        RoleSessionName="polecat",
-    )
+        # start w/credentials in environment tied to fred hutch account
+        sts = boto3.client("sts")
 
-    # get ec2 client in cortex production account
-    ec2 = boto3.client(
-        "ec2",
-        aws_access_key_id=resp["Credentials"]["AccessKeyId"],
-        aws_secret_access_key=resp["Credentials"]["SecretAccessKey"],
-        aws_session_token=resp["Credentials"]["SessionToken"],
-        region_name="us-west-2",
-    )
+        # assume role in cortex production account
+        # This role has both full administrative access AND the
+        # fh_disable_network_modification policy attached.
+        resp = sts.assume_role(
+            RoleArn="arn:aws:iam::329997391649:role/admin-with-limited-network",
+            RoleSessionName="polecat",
+        )
 
-    # try to create a vpc
-    print("Trying to create a VPC...")
-    try:
-        ec2.create_vpc(CidrBlock="", DryRun=True)
-        print("FAIL: able to create VPC")
-    except botocore.exceptions.ClientError as exc:
-        if "UnauthorizedOperation" in str(exc):
-            print("SUCCESS: unable to create VPC")
-        else:
-            print("FAIL: Got a different client error")
-    except Exception as exc:  # pylint: disable=broad-except
-        print("FAIL: Got a different error: {}".format(str(exc)))
+        # get ec2 client in cortex production account
+        ec2 = boto3.client(
+            "ec2",
+            aws_access_key_id=resp["Credentials"]["AccessKeyId"],
+            aws_secret_access_key=resp["Credentials"]["SecretAccessKey"],
+            aws_session_token=resp["Credentials"]["SessionToken"],
+            region_name="us-west-2",
+        )
+
+        # try to create a vpc
+        try:
+            ec2.create_vpc(CidrBlock="", DryRun=True)
+            self.fail("able to create VPC")
+        except botocore.exceptions.ClientError as exc:
+            self.assertTrue("UnauthorizedOperation" in str(exc))
+        except Exception as exc:  # pylint: disable=broad-except
+            self.fail("Got an unexpected error: {}".format(str(exc)))
 
 
 if __name__ == "__main__":
-    main()
+    unittest.main(warnings="ignore")
